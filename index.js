0194f93d6a6fdb3e59c38351fe4b06863f8e5cde
jfkbeatkid <hernadezrigoberto@gmail.com>
2017-06-17 16:42:30 -0400

    fleshed out DB server with todo model and got connection working

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv');
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8080;
+const mongoURI = process.env.MONGOURI || require('./secrets').MONGOURI;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(mongoURI);
+
+server.get('/', (req, res, next) => {
+  res.send('BOOYA');
+})
+
+server.listen(8080, () => {
+  console.log('Now listening on port:', port);
+})
---
+++ models/todo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: false
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: Array,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+todoSchema.pre('findOneAndUpdate', function(){
+  this.update({}, { $set: { updated: Date.now() }})
+})
+
+export const Todo = mongoose.model('Todo', todoSchema);
