ef68737ca67f53a3ff91cc65ca721460dadde79e
jfkbeatkid <hernadezrigoberto@gmail.com>
2017-07-08 17:58:14 -0400

    todos router works now

--- routers/todo.router.js
+++ routers/todo.router.js
@@ -3,27 +3,27 @@ const router = express.Router();
 const Todo = require('../models/todo.js');
 
 const errHandler = (err) => res.status(500).json({ msg: err });
-const successMsgHandler = (key = 'msg', value = 'success') => res.status(200).json({key, value});
+const successMsgHandler = (res, key = 'msg', value = 'success') => res.status(200).json({key, value});
 
 router.get('/todos', (req, res) => {
-  Todo.find({}, (err, todos) => err ? errHandler : successMsgHandler('todos', todos));
+  Todo.find({}, (err, todos) => err ? errHandler : successMsgHandler(res, 'todos', todos));
 });
 
 router.get('/todos/:todoId', (req, res) => {
-  Todo.find({_id: req.params.todoId}, (err, todos) => err ? errHandler : successMsgHandler('todos', todos));
+  Todo.find({_id: req.params.todoId}, (err, todos) => err ? errHandler : successMsgHandler(res, 'todos', todos ));
 });
 
 router.post('/todos', (req, res) => {
   let todo = new Todo(req.body);
-  todo.save(err => err ? errHandler : successMsgHandler());
+  todo.save(err => err ? errHandler : successMsgHandler(res));
 });
 
 router.put('/todos/:todoId', (req, res) => {
-  Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, (err, todo) => err ? errHandler : successMsgHandler());
+  Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, (err, todo) => err ? errHandler : successMsgHandler(res));
 });
 
 router.delete('/todos/:todoId', (req, res) => {
-  Todo.remove({_id: req.params.todoId}, err => err ? errHandler : successMsgHandler());
+  Todo.remove({_id: req.params.todoId}, err => err ? errHandler : successMsgHandler(res));
 })
 
 module.exports = router;
